1e6b70286bb507c0630da8d3e1653604354d86f8
kcksteve <60323908+kcksteve@users.noreply.github.com>
2022-09-14 22:45:16 -0400

    Added more to construct object
    filled out the required fields and translation population code.
    Todo: Flatten this data so children can inherit more easily without needing deep merge.

--- src/Pixi/PixiHelpers/PixiObjectConstructor.js
+++ src/Pixi/PixiHelpers/PixiObjectConstructor.js
@@ -19,13 +19,59 @@ class PixiObjectConstructor {
 
     #constructSprite(pixiObject, parent) {
         let object = new PIXI.Sprite(this.pixiResources[pixiObject.name].texture);
-        object.anchor.x = 0.5;
-        object.anchor.y = 0.5;
-        object.scale.x = 1;
-        object.scale.y = 1;
-        object.x = 0;
-        object.y = 0;
+
+        this.#applyTranslation(pixiObject, object);
+        object.name = pixiObject.name;
+        object.id = pixiObject.id;
         parent.addChild(object);
+
+        console.log(object);
+
+        if (pixiObject.hasOwnProperty('children')) {
+            this.#constructSprite(parent)
+        }
+    }
+
+    #applyTranslation(pixiObject, object) {
+        let anchorX = 0.5;
+        let anchorY = 0.5;
+        let scaleX = 1;
+        let scaleY = 1;
+        let positionX = 0;
+        let positionY = 0;
+        let positionZ = 0;
+        let angle = 0;
+
+        if (pixiObject.hasOwnProperty('translation')) {
+            if (pixiObject.translation.hasOwnProperty('anchor')) {
+                anchorX = pixiObject.translation.anchor.x;
+                anchorY = pixiObject.translation.anchor.y;
+            }
+
+            if (pixiObject.translation.hasOwnProperty('scale')) {
+                scaleX = pixiObject.translation.scale.x;
+                scaleY = pixiObject.translation.scale.y;
+            }
+
+            if (pixiObject.translation.hasOwnProperty('position')) {
+                positionX = pixiObject.translation.position.x;
+                positionY = pixiObject.translation.position.y;
+            }
+
+            if (pixiObject.translation.hasOwnProperty('angle')) {
+                angle = pixiObject.translation.angle;
+            }
+        }
+
+        object.sortableChildren = true;
+        object.anchor.x = anchorX;
+        object.anchor.y = anchorY;
+        object.scale.x = scaleX;
+        object.scale.y = scaleY;
+        object.position.x = positionX;
+        object.position.y = positionY;
+        object.zIndex = positionZ;
+        object.angle = angle;
     }
 }
 
--- src/Pixi/pixiObjects.js
+++ src/Pixi/pixiObjects.js
@@ -5,7 +5,23 @@ const PIXIOBJECTS = [
         id: 0,
         type: 'sprite',
         name: 'sonic',
-        image: sonicImg
+        image: sonicImg,
+        translation: {
+            anchor: {
+                x: 0.5,
+                y: 0.5
+            },
+            scale: {
+                x: 2,
+                y: 2
+            },
+            position: {
+                x: 100,
+                y: 0,
+                z: 0
+            },
+            angle: 0
+        }
     }
 ]
 
