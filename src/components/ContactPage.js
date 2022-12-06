456023aefa55b6b58b17e4342fc2e854cce39569
kcksteve <60323908+kcksteve@users.noreply.github.com>
2022-12-05 21:13:41 -0500

    Contact page completed
    added links on contact page and common css classes with scaling

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,8 +1,22 @@
 const ContactPage = () => {
     return(
         <div className='mainPage generalFadeIn'>
-            <div className='nameDiv'>
-                CONTACT
+            <div className='commonDiv'>
+                <div>
+                    LET'S GET IN TOUCH!
+                    <div className='commonTitle'>
+                        <a href='mailto:kcksteve@gmail.com'>kcksteve@gmail.com</a>
+                    </div>
+                    <div className='commonTitle'>
+                        <a href='tel:9055208195'>905-520-8195</a>
+                    </div>
+                    <div className='commonTitle'>
+                        <a href='https://www.linkedin.com/in/steve-krawczyk-6568601b9/'>LinkedIn</a>
+                    </div>
+                    <div className='commonTitle'>
+                        <a href='https://github.com/kcksteve'>Github</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
