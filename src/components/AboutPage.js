53366939d5ba5c711a4c769da110066b892dbbaa
kcksteve <60323908+kcksteve@users.noreply.github.com>
2022-12-19 21:12:42 -0500

    Update AboutPage.js
    Starting layout of about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,8 +1,31 @@
 const AboutPage = () => {
     return(
         <div className='mainPage generalFadeIn'>
-            <div className='nameDiv'>
-                ABOUT
+            <div className='commonDiv'>
+                <div>
+                    MORE ABOUT ME
+                    <div className='commonTitle'>
+                        I create compelling interactive digital experiences.
+                    </div>
+                    <div className='commonTitle'>
+                        Highly creative with experience in a broad range of software and physical development disciplines, I can help you bring your idea into reality.
+                    </div>
+                    <div className='commonTitle'>
+                        EXPERIENCE
+                    </div>
+                    <div className='commonTitle'>
+                        Design Lead
+                    </div>
+                    <div className='commonTitle'>
+                        PVZ Battle For Neighborville - Development Support
+                    </div>
+                    <div className='commonTitle'>
+                        ERP Specialist
+                    </div>
+                    <div className='commonTitle'>
+                        CERTIFICATIONS
+                    </div>
+                </div>
             </div>
         </div>
     )
